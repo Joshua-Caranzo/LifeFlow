9165088929c49bb306b32c0b4f0fe5313292d3e7
Beatrice <beatriceabigailalindao@gmail.com>
2025-12-31 17:27:13 +0800

    minor fixes

--- app/savings/page.tsx
+++ app/savings/page.tsx
@@ -247,7 +247,7 @@ export default function Savings() {
 
           <div className="flex flex-wrap gap-2">
             <select
-              value={selectedYear}
+              value={selectedYear || 2026}
               onChange={(e) => setSelectedYear(parseInt(e.target.value))}
               className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
